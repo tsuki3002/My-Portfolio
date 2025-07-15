91f2858430010f7bede84945089327f2d3bd64fe
Chinni K <chinni@Chinnis-MacBook-Air-2.local>
2025-07-15 14:15:59 -0500

    updated resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Sushma_Resume.pdf"
+                href="/Sushma_Kummari-Data-Analyst.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
